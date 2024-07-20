84d332946896f08fe8a4b2dbffe5782da1e49f0b
Gage-Morgan <gagemorgan206@gmail.com>
2024-07-19 23:29:09 -0500

    Update codebase

--- cards/deck.go
+++ cards/deck.go
@@ -7,8 +7,29 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+
+	// Enumeration Algorithm
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
-		fmt.Println("Index:", i, "|| Card:", card)
+		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {			// (deck, deck) means we want to return 2 values
+	return d[:handSize], d[handSize:]					// d[:handSize] returns the dealt hand
+														// d[handSize:] returns the cards remaining in the deck that havent been dealt
 }
